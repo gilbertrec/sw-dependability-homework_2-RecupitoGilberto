ef57dd95b449a65cdc04f32fee662b8655edd555
gilbertrec <gilbertrecupito@gmail.com>
2020-10-02 20:42:50 +0200

    End Session 2 Homework

--- smosa/math4j/src/test/java/org/magee/math/RationalTest.java
+++ smosa/math4j/src/test/java/org/magee/math/RationalTest.java
@@ -10,6 +10,7 @@ public class RationalTest {
     //This is the test class of Rational class
     //that exercise methods that do the arithmetic operations of a rational number
 
+
     //This test shows the bug in the add method that declares
     //an integer number as a rational with 0 as denominator
     //so it shouldn't launch the exception and pass the test here
@@ -28,7 +29,9 @@ public class RationalTest {
             verifyException("org.magee.math.Rational", e);
         }
     }
-    //This test instead checks correctly the launch of the exception "NumberFormatException
+
+    //This test instead checks correctly the launch of the exception "NumberFormatException"
+    //during the execution of the method add()
     @Test
     public void NumberFormatExceptionTest()  throws Throwable  {
         Rational rational0 = new Rational(762L, 762L);
@@ -47,6 +50,7 @@ public class RationalTest {
 
     // Test for the add method with a Rational Object and it shows the following bug:
     // the math operational order is not respected, uses pars.
+    //this test also exercise the method doubleValue()
     @Test
     public void addTestWithRationalObject() throws Throwable{
         Rational rational0 = new Rational(1L,1L);
@@ -54,13 +58,15 @@ public class RationalTest {
         assertEquals(1.5,rational0.doubleValue(),0.01);
     }
 
+    //This test exercise the add() and the longValue() method
     // this test shows the bug in the add method with an integer parameter that an integer is declared as
     // a rational with 0 as denominator, instead is 1
     @Test
     public void addTestWithInteger() throws Throwable{
         Rational rational0 = new Rational(1L,1L);
         rational0.add(1L);
-        assertEquals(2,rational0.longValue());
+        assertEquals("Numerator value expected different",2L,rational0.numerator);
+        assertEquals("Long Value expected different",2,rational0.longValue());
     }
 
     // Test that exercises the methods longValue() and doubleValue()
@@ -85,6 +91,7 @@ public class RationalTest {
     //multiplying a rational number for its self
     //This test was incorrect because of the bug in multiply method
     //changed the second assertion with the real expected value
+    //This test also exercise the longValue() and the floatValue() method
     @Test
     public void test03()  throws Throwable  {
         Rational rational0 = new Rational((-1431L), (-1431L));
@@ -148,7 +155,7 @@ public class RationalTest {
     // This test shows a bug. I think that the problem is
     // the function control "if the numerator is less than 1",
     @Test
-    public void reduceTest() throws Throwable {
+    public void reduceTestInteger() throws Throwable {
         Rational rational = new Rational (8L,8L);
         Rational result = rational.reduce();
         assertEquals("Numerator expected different",1L,result.numerator);
@@ -156,7 +163,7 @@ public class RationalTest {
     }
 
     @Test
-    public void reduceTest2() throws Throwable {
+    public void reduceTestNonInteger() throws Throwable {
         Rational rational = new Rational (2L,8L);
         Rational result = rational.reduce();
         assertEquals("Numerator expected different",1L,result.numerator);
@@ -205,7 +212,6 @@ public class RationalTest {
     }
 
     //This test exercise the method abs()
-    //this method has a bug in the denominator condition.
     @Test
     public void absTestNegative()  throws Throwable  {
         Rational rational0 = new Rational(-1L, (1802L));
@@ -215,7 +221,6 @@ public class RationalTest {
     }
 
     //This test exercise the method abs()
-    //this method has a bug in the denominator condition.
     @Test
     public void absTestPositive()  throws Throwable  {
         Rational rational0 = new Rational(1L, (1802L));
@@ -223,24 +228,24 @@ public class RationalTest {
         assertEquals("Resulting Numerator expected different",1L, rational1.numerator);
         assertEquals("Resulting Denominator expected different",(1802L), rational1.denominator);
     }
-    // This method exercise the method negate()
-    // 
+    // This method exercise the method negate() with a positive rational number
     @Test
     public void test08()  throws Throwable  {
         Rational rational0 = new Rational(887L, 887L);
         Rational rational1 = rational0.negate();
-        assertEquals((byte) (-1), rational1.byteValue());
-        assertEquals(1, rational0.intValue());
+        assertEquals("Negation Rational value expected different",(byte) (-1), rational1.byteValue());
+        assertEquals("Input Rational value expected different",1, rational0.intValue());
     }
-
+    // This method exercise the method negate() with a negative rational number
     @Test
-    public void test09()  throws Throwable  {
-        Rational rational0 = new Rational(0L, 1090L);
-        double double0 = rational0.doubleValue();
-        assertEquals(1090L, rational0.denominator);
-        assertEquals(0.0, double0, 0.01);
+    public void negateTestNegative()  throws Throwable  {
+        Rational rational0 = new Rational(-887L, 887L);
+        Rational rational1 = rational0.negate();
+        assertEquals("Negation Rational value expected different",(byte) (1), rational1.byteValue());
+        assertEquals("Input Rational value expected different",-1, rational0.intValue());
     }
 
+    // This method exercise the method abs() with a positive rational non-integer number
     @Test
     public void test10()  throws Throwable  {
         Rational rational0 = new Rational(1184L, 73L);
@@ -248,15 +253,12 @@ public class RationalTest {
         assertEquals(1184L, rational0.numerator);
         assertEquals(16.21917808219178, rational1.doubleValue(), 0.01);
     }
+
+    // This test exercise the method byteValue()
+    //this additional test shows an "out of range"
+    @Test
+    public void testByteValue() throws Throwable {
+        Rational rational0 = new Rational(7721L,1L);
+        assertEquals(7772L,rational0.byteValue());
+    }
 }
-
-
-//Other observations:
-/*
-1)
- I think it's better to use the same method in the several tests for getting the value
- for the assertion and then test singularly each get method,
- but the tool exercises the code in this way in order to
- maximize the statement coverage and minimize the number of tests,
- and in this way it exercises each single method without additional tests
- */
